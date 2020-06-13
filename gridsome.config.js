f580b021d59c68e68d971da7b83fdbd01e5fc3c6
Alan Jenkins <alanjenkins1978@gmail.com>
2020-06-13 14:00:59 +0100

    Replace tailwind manual setup with gridsome plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,20 +1,14 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-const tailwind = require('tailwindcss');
-
-const postcssPlugins = [tailwind('./tailwind.config.js')];
-
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss());
-
 module.exports = {
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
-    },
-  },
   siteName: 'Cards by Chris',
   siteDescription: 'Hand crafted cards and memory journals by Chris Wilson',
   titleTemplate: `%s`,
-  plugins: [],
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+
+      options: {
+        tailwindConfig: './tailwind.config.js',
+      },
+    },
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ['./src/**/*.vue', './src/**/*.js'],
+    options: {
+      whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+      ],
+    },
+  },
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
